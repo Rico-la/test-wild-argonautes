2ea1cbca4b0a32d1fd7bd8088a97b49500682532
RicoQuill <rico.alves91@gmail.com>
2021-05-24 22:35:19 +0200

    fix url database

--- app/database.js
+++ app/database.js
@@ -9,10 +9,17 @@ const { Client } = require('pg');
 
 //* On crée une instance
 //* Cette instance est notre lien JS vers la BDD
-const client = new Client();
+const client = new Client({
+    connectionString: process.env.PG_URL,
+    ssl: {
+        rejectUnauthorized: false
+      }
+});
 
 //* On connecte le client pour le rendre prêt à l'emploi
 client.connect();
+
+console.log(client);
 
 //* On place le client connecté dans l'export pour le rendre disponible dans d'autres fichiers de notre appli
 module.exports = client;
